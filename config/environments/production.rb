a6be894da8c4dff57a881f3b863b6bede9a0e4cb
Akihiro Watanabe <watakihiro@gmail.com>
2016-11-17 04:57:43 +0000

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
     config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'fierce-scrubland-13315..herokuapp.com'
+  host = 'fierce-scrubland-13315.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
