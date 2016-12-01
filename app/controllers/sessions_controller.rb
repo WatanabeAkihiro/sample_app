2d05e99955dbc4319f8cd91bab7aca973ef1f728
Akihiro Watanabe <watakihiro@gmail.com>
2016-12-01 05:02:58 +0000

    ブランチを切り替えるためにコミット

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -20,11 +20,23 @@ class SessionsController < ApplicationController
       render 'new'
       flash.now[:danger]='Invalid email/password combination'
     end
+      if (request.path_info == "/auth/twitter/callback")
+      auth = request.env["omniauth.auth"]
+      session[:oauth_token] = auth.credentials.token
+      session[:oauth_token_secret] = auth.credentials.secret
+      session[:username] = auth.extra.access_token.params[:screen_name]
+      redirect_to "/tweet", :notice => "サインイン！"
+    end
   end
   
   def destroy
     log_out if logged_in?
-    redirect_to root_url
+    session[:oauth_token] = nil
+    session[:oauth_token_secret] = nil
+    redirect_to root_url, :notice => "サインアウト！"
+    if (request.path_info == "/signout_twitter")
+      session[:username] = nil
+    end
   end
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161109133739) do
+ActiveRecord::Schema.define(version: 20161128065555) do
+
+  create_table "activities", force: :cascade do |t|
+    t.integer  "trackable_id"
+    t.string   "trackable_type"
+    t.integer  "owner_id"
+    t.string   "owner_type"
+    t.string   "key"
+    t.text     "parameters"
+    t.integer  "recipient_id"
+    t.string   "recipient_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "micropost_id"
+    t.text     "comment"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "micropost_id"
+    t.integer "user_id"
+    t.boolean "is_like"
+  end
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "picture"
+    t.integer  "likes_count"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
@@ -35,6 +67,16 @@ ActiveRecord::Schema.define(version: 20161109133739) do
   add_index "relationships", ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
   add_index "relationships", ["follower_id"], name: "index_relationships_on_follower_id"
 
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "comment_id"
+    t.integer  "micropost_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "taggings", ["comment_id"], name: "index_taggings_on_comment_id"
+  add_index "taggings", ["micropost_id"], name: "index_taggings_on_micropost_id"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
@@ -48,6 +90,9 @@ ActiveRecord::Schema.define(version: 20161109133739) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.text     "introduction"
+    t.text     "location"
+    t.text     "website"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
