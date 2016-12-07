ff6cce3679d6f1f164e4293b7ae9e5b79df59200
Akihiro Watanabe <watakihiro@gmail.com>
2016-12-07 02:13:12 +0000

    Add twitter-api

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -21,12 +21,12 @@ class SessionsController < ApplicationController
       flash.now[:danger]='Invalid email/password combination'
     end
       if (request.path_info == "/auth/twitter/callback")
-      auth = request.env["omniauth.auth"]
-      session[:oauth_token] = auth.credentials.token
-      session[:oauth_token_secret] = auth.credentials.secret
-      session[:username] = auth.extra.access_token.params[:screen_name]
-      redirect_to "/tweet", :notice => "サインイン！"
-    end
+        auth = request.env["omniauth.auth"]
+        session[:oauth_token] = auth.credentials.token
+        session[:oauth_token_secret] = auth.credentials.secret
+        session[:username] = auth.extra.access_token.params[:screen_name]
+        redirect_to "/tweet", :notice => "サインイン！"
+      end
   end
   
   def destroy
